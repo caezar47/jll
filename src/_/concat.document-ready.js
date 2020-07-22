254409b7d22be8a8686f34d5d32227ac1af361f5
caezar47 <iss47@ya.ru>
2020-07-22 12:50:17 +0300

    Добавил модификатор

--- src/_/concat.document-ready.js
+++ src/_/concat.document-ready.js
@@ -1,2 +1,3 @@
 var select_default=$("[data-select]");select_default.select2({minimumResultsForSearch:-1});
-1199<$(document).width()&&$(".form__card.is--chart").sticky({topSpacing:88});
+
+1199<$(document).width()&&$(".form__sticky").sticky({topSpacing:88});
--- src/block/plugin/sticky/.document-ready.js
+++ src/block/plugin/sticky/.document-ready.js
@@ -1,3 +1,3 @@
 if($(document).width() > 1199) {
-	$(".form__card.is--chart").sticky({topSpacing:88});
+	$(".form__sticky").sticky({topSpacing:88});
 } 
