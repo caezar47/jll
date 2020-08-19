502db60d417c09a20944d8adc6727db36f561c1c
caezar47 <iss47@ya.ru>
2020-08-19 16:17:14 +0300

    Доработал график

--- src/_/concat.document-ready.js
+++ src/_/concat.document-ready.js
@@ -1,3 +1,2 @@
-var select_default=$("[data-select]");select_default.select2({minimumResultsForSearch:-1});
-
 1199<$(document).width()&&$(".form__sticky").sticky({topSpacing:88});
+var select_default=$("[data-select]");select_default.select2({minimumResultsForSearch:-1});
